0d149802443fdc38675452e059e6144d70a10fc1
mos <77763580+TheMosKau@users.noreply.github.com>
2024-12-21 20:52:52 +0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
   title: 'Tuah',
-  email: 'mail@tuah.link',
-  github: 'https://github.com/ccbikai/sink',
+  email: '',
+  github: '',
   twitter: '',
   telegram: '',
   mastodon: '',
   blog: '',
-  description: 'Link shortener based on Sink..',
+  description: 'a link shortener based on Sink.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
